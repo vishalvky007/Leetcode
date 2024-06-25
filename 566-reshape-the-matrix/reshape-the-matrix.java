0a6efb186e2b8aca58bd093897ccba4066535c0c
VISHAL KUMAR YADAV <9512278k@gmail.com>
2024-06-26 00:14:39 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (80.26%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        if(m*n != r*c) {
+            return mat;
+        }
+
+        int[][] reshapedMatrix = new int[r][c];
+        int row = 0, col = 0;
+
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                reshapedMatrix[row][col] = mat[i][j];
+                col++;
+                if(col == c) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+        return reshapedMatrix;
+    }
+}
