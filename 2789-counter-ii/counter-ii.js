67e6ee6d35d122f3c9b7fb624d63b3e99f1ad18c
VISHAL KUMAR YADAV <9512278k@gmail.com>
2024-07-03 01:08:48 +0530

    Time: 64 ms (30.79%) | Memory: 51.6 MB (54.08%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+
+    let originalVal = init;
+
+    function increment() {
+        return ++originalVal;
+    }
+    function decrement() {
+        return --originalVal;
+    }
+    function reset() {
+        return (originalVal = init);
+    }
+
+    return {increment, decrement, reset};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
