c790ad89767c520fb87d4369054843015a0679ed
VISHAL KUMAR YADAV <9512278k@gmail.com>
2024-07-20 13:03:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (84.89%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       
+        int start = 0, end = nums.length-1;
+        int mid = -1;    
+
+        while(start <= end) {
+            
+            mid = (start + end) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(target < nums[mid]) {
+                end = mid-1;
+            }
+            else {
+                start = mid + 1;
+            }    
+        }
+        
+        return end+1;
+
+    }
+}
