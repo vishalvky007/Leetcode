cb2945fe22abd560f6494391a2c68a1af5077c6e
VISHAL KUMAR YADAV <9512278k@gmail.com>
2024-07-25 22:46:39 +0530

    Time: 25 ms (71.55%) | Memory: 55.9 MB (57.59%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,50 @@
+class Solution {
+   // Main method to sort the array
+    public static int[] sortArray(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    // Recursive method to divide and sort the array
+    private static void mergeSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+            merge(nums, left, mid, right);
+        }
+    }
+
+    // Method to merge two sorted halves
+    private static void merge(int[] nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        System.arraycopy(nums, left, leftArray, 0, n1);
+        System.arraycopy(nums, mid + 1, rightArray, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                nums[k++] = leftArray[i++];
+            } else {
+                nums[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < n1) {
+            nums[k++] = leftArray[i++];
+        }
+
+        while (j < n2) {
+            nums[k++] = rightArray[j++];
+        }
+    }
+}
