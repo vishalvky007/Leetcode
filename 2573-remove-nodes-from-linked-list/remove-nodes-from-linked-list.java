c0e1aca17bfcef72b74c0fc2d4351799f31278e6
VISHAL KUMAR YADAV <9512278k@gmail.com>
2024-09-23 23:42:55 +0530

    Time: 4 ms (100.00%) | Memory: 62.9 MB (77.15%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -8,28 +8,62 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// Basic Solution
+// class Solution {
+//     public ListNode removeNodes(ListNode head) {
+//         Stack<ListNode> stack = new Stack<>();
+//         ListNode temp = head;
+//         while(temp != null) {
+//             stack.push(temp);
+//             temp = temp.next;
+//         }
+
+//         ListNode newHead = stack.pop();
+//         int maxVal = newHead.val;
+//         while(!stack.empty()) {
+//             ListNode node = stack.pop();
+//             if(node.val < maxVal) {
+//                 continue;
+//             } else {
+//                 // insert at head
+//                 node.next = newHead;
+//                 newHead = node;
+//                 maxVal = node.val;
+//             } 
+//         }
+//         return newHead;
+//     }
+// }
+
+// Optimised Solution
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        Stack<ListNode> stack = new Stack<>();
-        ListNode temp = head;
-        while(temp != null) {
-            stack.push(temp);
-            temp = temp.next;
+        // reverse the linked list
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nextNode = null;
+
+        while(curr != null) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
         }
 
-        ListNode newHead = stack.pop();
-        int maxVal = newHead.val;
-        while(!stack.empty()) {
-            ListNode node = stack.pop();
-            if(node.val < maxVal) {
-                continue;
-            } else {
-                // insert at head
-                node.next = newHead;
-                newHead = node;
+        // create new list
+        ListNode newList = new ListNode(prev.val);
+        curr = prev.next;
+        int maxVal = newList.val;
+        while(curr != null) {
+            if(curr.val>=maxVal) {
+                ListNode node = new ListNode(curr.val);
+                node.next = newList;
+                newList = node;
                 maxVal = node.val;
-            } 
+            }
+            curr = curr.next;
         }
-        return newHead;
+
+        return newList;
     }
 }
