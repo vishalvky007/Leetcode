8bfb3e89b66da2feeec672c23cfa28b66125a720
VISHAL KUMAR YADAV <9512278k@gmail.com>
2024-10-04 23:50:33 +0530

    Time: 2 ms (100.00%) | Memory: 55.1 MB (98.49%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -4,9 +4,11 @@ class Solution {
         int teams = n/2;
         int totalSum = 0;
 
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
+        // HashMap<Integer,Integer> freqMap = new HashMap<>();
+        int[] freqMap = new int[1001];
         for(int element : skill) {
-            freqMap.put(element, freqMap.getOrDefault(element,0)+1);
+            // freqMap.put(element, freqMap.getOrDefault(element,0)+1);
+            freqMap[element]++;
             totalSum += element;
         }
         if(totalSum % teams != 0) {
@@ -16,15 +18,19 @@ class Solution {
         int targetPoints = totalSum / teams;
         long res = 0;
         for(int element : skill) {
-            if(freqMap.get(element)==0) {
+            // if(freqMap.get(element)==0) {
+            if(freqMap[element] == 0) {
                 continue;
             }
-            freqMap.put(element, freqMap.get(element)-1);
+            // freqMap.put(element, freqMap.get(element)-1);
+            freqMap[element]--;
             int partner = targetPoints - element;
-            if(!freqMap.containsKey(partner) || freqMap.get(partner) == 0) {
+            // if(!freqMap.containsKey(partner) || freqMap.get(partner) == 0) {
+            if(freqMap[partner] == 0) {
                 return -1;
             }
-            freqMap.put(partner, freqMap.get(partner)-1);
+            // freqMap.put(partner, freqMap.get(partner)-1);
+            freqMap[partner]--;
             res = res + ((long)element * (long)partner); 
         }
         return res;
