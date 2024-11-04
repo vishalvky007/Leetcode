561fbcde5bcbb15253919e8fe83d88178bfdd590
VISHAL KUMAR YADAV <9512278k@gmail.com>
2024-11-04 23:36:17 +0530

    Time: 17 ms (80.51%) | Memory: 45.8 MB (59.79%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String compressedString(String word) {
+        StringBuilder comp = new StringBuilder();
+
+        int n = word.length();
+        int i = 0;
+        while(i < n) {
+            int count = 0;
+            char ch = word.charAt(i);
+            while(i<n && count < 9 && word.charAt(i) == ch) {
+                count++;
+                i++;
+            }
+            comp.append(count);
+            comp.append(ch);
+        }
+        return String.valueOf(comp);
+    }
+}
