ef9d89ee79c95662e787d1f0aed559e124eb2442
VISHAL KUMAR YADAV <9512278k@gmail.com>
2024-11-09 23:52:36 +0530

    Time: 1739 ms (17.86%) | Memory: 41 MB (36.54%) - LeetSync

---
+++ 3394-minimum-array-end/minimum-array-end.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long minEnd(int n, int x) {
+        long ans = x;
+        n-=1;
+        while(n-- > 0) {
+            ans = (ans+1) | x;
+        }
+        return ans;
+    } 
+}
